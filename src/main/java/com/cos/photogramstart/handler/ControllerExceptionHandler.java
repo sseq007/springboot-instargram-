229212335220f8dcc86a32b77f51b586b424ea09
sseq007 <sseq007@naver.com>
2022-08-09 16:11:58 +0900

    회원가입시 전처리 유효성 검사(공통 에러확인 응답 DTO)

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.web.dto.CMRespDto;
 
 @RestController
 @ControllerAdvice // 낚아채기
 public class ControllerExceptionHandler {
 
 	@ExceptionHandler(CustomValidationException.class)
-	public Map<String, String> validationException(CustomValidationException e) {
-		return e.getErrorMap();
+	public CMRespDto<?> validationException(CustomValidationException e) {
+		return new CMRespDto<Map<String,String>>(-1, e.getMessage(), e.getErrorMap());
 	}
 }
