d44a52d8590207efa3a1a26efe9cc891c4f4b656
sseq007 <sseq007@naver.com>
2022-08-09 16:33:33 +0900

    회원가입시 전처리 유효성 검사(공통 응답 Script)

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -1,20 +1,25 @@
 package com.cos.photogramstart.handler;
 
-import java.util.Map;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
-import com.cos.photogramstart.web.dto.CMRespDto;
+import com.cos.photogramstart.util.Script;
+
 
 @RestController
 @ControllerAdvice // 낚아채기
 public class ControllerExceptionHandler {
 
 	@ExceptionHandler(CustomValidationException.class)
-	public CMRespDto<?> validationException(CustomValidationException e) {
-		return new CMRespDto<Map<String,String>>(-1, e.getMessage(), e.getErrorMap());
+	public String validationException(CustomValidationException e) {
+	
+		//CMRespDto, Script 비교
+		// 1. 클라이언트에게 응답할 때는 Script 좋음
+		// 2. Ajax통신 - CMRespDto
+		// 3. Android 통신 - CMRespDto
+		return Script.back(e.getErrorMap().toString());
 	}
 }
