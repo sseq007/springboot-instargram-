38c4e1732ab5bb40e41d5e50b4d793f23f59bd6a
sseq007 <sseq007@naver.com>
2022-08-26 15:55:32 +0900

    프로필 페이지 - 양방향 메핑 이용하여 뷰 렌더링중

--- src/main/java/com/cos/photogramstart/domain/user/User.java
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -1,12 +1,17 @@
 package com.cos.photogramstart.domain.user;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
+
+import com.cos.photogramstart.domain.image.Image;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,10 +27,13 @@ import lombok.NoArgsConstructor;
 @Entity // DB에 테이블을 생성
 public class User {
 
+	
 	@javax.persistence.Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 데이터베이스를 따라간다
 	private int Id;
 	
+	
+	 
 	@Column( length = 20, unique = true)
 	private String username;
 	@Column(nullable = false)
@@ -42,6 +50,13 @@ public class User {
 	
 	private String profileImageUrl; //사진
 	private String role; //권한
+	//나는 연관관계의 주인이 아니다 테이블에 컬럼을 만들지마
+	//user를 Select 할때 해당 User id로 등록된 image들을 가져와
+	//Lazy = User를 Select할 때 해당 User Id로 등록된 images들을 가져오지마 - 대신 getimages() 함수가 호출될 때 가져와
+	//EAGER = User를 Selec할 때 해당 User id로 등록된 images들을 전부 Join해서 가져와
+	 @OneToMany(mappedBy = "user", fetch= FetchType.EAGER) 
+	private List<Image> images; // 양방향 메핑
+	
 	private LocalDateTime createDate;
 	
 	@PrePersist // DB에 INSERT 되기 직전에 실행
--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomApiException;
+import com.cos.photogramstart.handler.ex.CustomException;
 import com.cos.photogramstart.handler.ex.CustomValidationApiException;
 import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.util.Script;
@@ -33,6 +34,13 @@ public class ControllerExceptionHandler {
 		
 	}
 	
+	@ExceptionHandler(CustomException.class)
+	public String exception(CustomException e) {
+		
+			return Script.back(e.getMessage());
+		
+	}
+	
 	@ExceptionHandler(CustomValidationApiException.class)
 	public ResponseEntity<?> validationException(CustomValidationApiException e) {
 	
@@ -40,7 +48,7 @@ public class ControllerExceptionHandler {
 	}
 	
 	@ExceptionHandler(CustomApiException.class)
-	public ResponseEntity<?> 메ㅑException(CustomApiException e) {
+	public ResponseEntity<?> customApiException(CustomApiException e) {
 	
 		return new ResponseEntity<>(new CMRespDto<>(-1,e.getMessage(),null), HttpStatus.BAD_REQUEST);
 	}
