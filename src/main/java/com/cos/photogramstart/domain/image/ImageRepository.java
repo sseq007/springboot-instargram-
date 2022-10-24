e0b5da6f8136c841140e55aefa2d59b280bc07b1
sseq007 <sseq007@naver.com>
2022-10-25 04:01:47 +0900

    기타 - 인기 페이지 구현 완료 '

--- src/main/java/com/cos/photogramstart/domain/image/ImageRepository.java
+++ src/main/java/com/cos/photogramstart/domain/image/ImageRepository.java
@@ -12,8 +12,8 @@ public interface ImageRepository extends JpaRepository<Image, Integer> {
 	@Query(value="SELECT * FROM image WHERE userId IN (SELECT toUserId FROM subscribe WHERE fromUserId = :principalId) ORDER BY id DESC",nativeQuery = true)
 	Page<Image> mStroy(int principalId,Pageable pageable);
 	
-	
-	
+	@Query(value="SELECT i.* FROM image i INNER JOIN (SELECT imageId,COUNT(imageId) as likeCount FROM likes GROUP BY imageId ) c ON i.id = c.imageId ORDER BY likeCount desc",nativeQuery = true)
+	List<Image> mPopular();
 	
 	
 }
